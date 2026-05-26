09b1e41d3d41eaed8a54f666efb17d5fe1860f74
joyces20 <mischaellakenyojoyces@gmail.com>
2026-05-26 18:29:49 +0100

    feat: configuration de base,dossier src,dependances et liaisons firebase androi

---
+++ src/services/firebase.js
@@ -0,0 +1,6 @@
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
+import messaging from '@react-native-firebase/messaging';
+
+export { auth, firestore, storage, messaging };
